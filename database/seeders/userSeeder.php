afdfdb145010e2fc0ad4761d0dd93362311e5c5f
Brian Saputra <briansaputra078@gmail.com>
2024-08-06 22:49:32 +0800

    Menambahkan database users from seeder

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class userSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class userSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            ['username' => 'admin', 'slug' => 'admin', 'email' => 'admin@gmail.com', 'password' => bcrypt('Admin123'), 'phone' => '082150823875', 'address' => 'Kalimantan Selatan, Banjarmasin, Kayu Tangi 2'],
+        ];
+
+        DB::table('users')->insert($data);
     }
 }
